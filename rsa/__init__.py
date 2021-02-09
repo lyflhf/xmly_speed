4ff1cbbe8fdea7dbb4f6eeb3f362630eb90fc7f7
lyflhf <39027194+lyflhf@users.noreply.github.com>
2021-02-09 22:21:30 +0800

    Add files via upload

--- rsa/__init__.py
+++ rsa/__init__.py
@@ -1,39 +1,42 @@
-＃-*-编码：utf-8-*-
-＃
-＃版权所有2011 SybrenA.Stüvel<sybren@stuvel.eu>
-＃
-＃根据Apache许可证2.0版（“许可证”）获得许可；
-＃除非符合许可，否则您不得使用此文件。
-＃您可以在以下位置获得许可的副本
-＃
-＃https://www.apache.org/licenses/LICENSE-2.0
-＃
-＃除非适用法律要求或书面同意，否则软件
-根据许可协议分发的＃是按“原样”分发的，
-＃没有任何明示或暗示的保证或条件。
-＃有关特定语言的管理权限，请参阅许可证。
-＃许可中的限制。
-“”“ RSA模块
-用于计算大素数以及RSA加密，解密，签名的模块
-和验证。包括生成公钥和私钥。
-警告：此实现不使用将明文输入压缩为
-防止重复或其他常见的安全性改进。小心使用。
-“”
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2011 Sybren A. Stüvel <sybren@stuvel.eu>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""RSA module
 
-来自 rsa。密钥 导入 newkeys，PrivateKey，PublicKey
-来自 rsa。pkcs1 导入 加密，解密，签名，验证，DecryptionError，
-    VerificationError，find_signature_hash，   sign_hash，compute_hash
+Module for calculating large primes, and RSA encryption, decryption, signing
+and verification. Includes generating public and private keys.
 
-__author__  =  “ Sybren Stuvel，Barry Mead和Yesudeep Mangalapilly”
-__date__  =  “ 2018-09-16”
-__version__  =  '4.0'
+WARNING: this implementation does not use compression of the cleartext input to
+prevent repetitions, or other common security improvements. Use with care.
 
-＃如果我们直接运行就进行doctest
-如果 __name__  ==  “ __main__”：
-    导入 doctest
+"""
 
-    doctest。testmod（）
+from rsa.key import newkeys, PrivateKey, PublicKey
+from rsa.pkcs1 import encrypt, decrypt, sign, verify, DecryptionError, \
+    VerificationError, find_signature_hash,  sign_hash, compute_hash
 
-__all__  = [[ “ newkeys”，“ encrypt”，“ decrypt”，“ sign”，“ verify”，“ PublicKey”，
-           'PrivateKey'，'DecryptionError'，'VerificationError'，
-           'compute_hash'，'sign_hash' ]
+__author__ = "Sybren Stuvel, Barry Mead and Yesudeep Mangalapilly"
+__date__ = "2018-09-16"
+__version__ = '4.0'
+
+# Do doctest if we're run directly
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+
+__all__ = ["newkeys", "encrypt", "decrypt", "sign", "verify", 'PublicKey',
+           'PrivateKey', 'DecryptionError', 'VerificationError',
+           'compute_hash', 'sign_hash']
